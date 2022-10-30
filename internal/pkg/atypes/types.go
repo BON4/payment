c2e3de7154fda3b2e73c915f9fbb166153607775
home <silvia.homam@gmail.com>
2022-10-30 21:28:06 +0200

    README

--- internal/pkg/atypes/types.go
+++ internal/pkg/atypes/types.go
@@ -4,6 +4,8 @@ import (
 	"database/sql/driver"
 	"time"
 
+	"fmt"
+
 	"github.com/shopspring/decimal"
 )
 
@@ -15,32 +17,24 @@ type DateTime struct {
 	time.Time
 }
 
-// All this to rune conversion is just to delete trailing zero in hour
-// Golang dont have specified format tor this
+// Golang dont have specified format tor h-mm-ss
 func (date *DateTime) MarshalCSV() (string, error) {
-	//s := []rune(date.Format("2006-01-02 15:04:05"))
-	//if date.Hour() < 10 {
-	//	s = delChar(s, len(s)-8)
-	//}
-	return date.Format("2006-01-02 15:04:05"), nil
+	return fmt.Sprintf("%d-%02d-%02d %01d:%02d:%02d",
+		date.Year(), date.Month(), date.Day(),
+		date.Hour(), date.Minute(), date.Second()), nil
 }
 
 func (date *DateTime) MarshalJSON() ([]byte, error) {
-	//s := []rune(date.Format("2006-01-02 15:04:05"))
-	//if date.Hour() < 10 {
-	//	s = delChar(s, len(s)-8)
-	//}
-	return []byte(`"` + date.Format("2006-01-02 15:04:05") + `"`), nil
+	return []byte(fmt.Sprintf(`"%d-%02d-%02d %01d:%02d:%02d"`,
+		date.Year(), date.Month(), date.Day(),
+		date.Hour(), date.Minute(), date.Second())), nil
 }
 
-// All this to rune conversion is just to delete trailing zero in hour
-// Golang dont have specified format tor this
+// Golang dont have specified format tor h-mm-ss
 func (date *DateTime) String() string {
-	//s := []rune(date.Format("2006-01-02 15:04:05"))
-	//if date.Hour() < 10 {
-	//	s = delChar(s, len(s)-8)
-	//}
-	return date.Format("2006-01-02 15:04:05")
+	return fmt.Sprintf("%d-%02d-%02d %01d:%02d:%02d",
+		date.Year(), date.Month(), date.Day(),
+		date.Hour(), date.Minute(), date.Second())
 }
 
 // Convert the CSV string as internal date
